28b04674ceaefb7992b18b6ac84b6003cb4df977
ancient225 <91169811+ancient225@users.noreply.github.com>
2022-09-07 08:59:56 +1000

    update for playtesting
    edited text and walls and added ability to exit platformer

--- IGB200/Assets/Scripts/Platformer/PlatformerCollision.cs
+++ IGB200/Assets/Scripts/Platformer/PlatformerCollision.cs
@@ -18,7 +18,10 @@ public class PlatformerCollision : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
     }
 
     private void OnTriggerEnter(Collider other)
