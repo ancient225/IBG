c2138c8fd56cf95f1951c12cf6bc2006601fe6a4
Vinnie-C-Bruv <82975078+Vinnie-C-Bruv@users.noreply.github.com>
2022-10-13 20:03:15 +1000

    fart music

--- IGB200/Assets/Scripts/Overworld/Text.cs
+++ IGB200/Assets/Scripts/Overworld/Text.cs
@@ -42,30 +42,42 @@ public class Text : MonoBehaviour
 
         if (UI == 0)
         {
+<<<<<<< Updated upstream
             info.text = "Hey Player, feeling up to exploring QUT today? Try playing a game of ping pong with Bob. Points: " + Points;
         }
         else if (UI == 1)
         {
             info.text = "Yo that's pre good! Now let's help out Pete with his chemistry homework. Points: " + Points;
+=======
+            info.text = "Hello! Welcome to QUT open day! Wanna play a game of basketball with my friend Bob from the QUT basketball club?";
+        }
+        else if (UI == 1)
+        {
+            info.text = "Wow, you're practically NBA material! Maybe you could also help my friend Pete whith his chemistry project?";
+>>>>>>> Stashed changes
         }
         else if (UI == 2)
         {
-            info.text = "Uni level chemistry has nothing on you! Maybe you could help out Fred with his robotics project? Points: " + Points;
+            info.text = "Uni level chemistry has nothing on you! Maybe you could help out Fred with his robotics project?";
         }
         else if (UI == 3)
         {
-            info.text = "and some more story - Go talk to Rebecca. Points: " + Points;
+            info.text = "Nice! My friend Rebecca needs help with her music project, maybe you could contribute a little?";
         }
         else if (UI == 4)
         {
+<<<<<<< Updated upstream
             info.text = "story - Congrates go check out the shop. Points: " + Points + "  Thanks for playing the demo later we are planning for better level design more mini games and models";
+=======
+            info.text = "Thanks for playing the demo later we are planning for better level design more mini games and models";
+>>>>>>> Stashed changes
         }
 
 
 
         if (UI == 99)
         {
-            info.text = "Did you know there are two different types of white blood cells? The b lymphocyte blood cells create antibodies and warn the t lymphocyts of invading cells, who then destroy the antibody.";
+            info.text = "Did you know that qut offers 23 seperate clubs for sports/recreation?";
 
             if (allow)
             {
@@ -75,7 +87,11 @@ public class Text : MonoBehaviour
         }
         else if (UI == 98)
         {
+<<<<<<< Updated upstream
             info.text = "Did you know 'interesting facts about certain field One part two' how fasinating.";
+=======
+            info.text = "Did you know QUT offers sports scholarships to people of all backgrounds, abilities and financial situations?";
+>>>>>>> Stashed changes
 
             if (allow)
             {
@@ -85,7 +101,11 @@ public class Text : MonoBehaviour
         }
         else if (UI == 97)
         {
+<<<<<<< Updated upstream
             info.text = "Did you know 'interesting facts about certain field Two Part one' how fasinating.";
+=======
+            info.text = "In addition to biology, chemistry and physics, QUT also offers classes based on earth and environmental science?";
+>>>>>>> Stashed changes
 
             if (allow)
             {
@@ -95,7 +115,11 @@ public class Text : MonoBehaviour
         }
         else if (UI == 96)
         {
+<<<<<<< Updated upstream
             info.text = "Did you know 'interesting facts about certain field Two part two' how fasinating.";
+=======
+            info.text = "Did you know there are two different types of white blood cells? The b lymphocyte blood cells create antibodies and warn the t lymphocyts of invading cells, who then destroy the antibody.";
+>>>>>>> Stashed changes
 
             if (allow)
             {
@@ -105,7 +129,11 @@ public class Text : MonoBehaviour
         }
         else if (UI == 95)
         {
+<<<<<<< Updated upstream
             info.text = "Did you know 'interesting facts about certain field Three Part one' how fasinating.";
+=======
+            info.text = "Did you know QUT had a free online resource that teaches robotics from kindergarten to undergraduate level? It's called robot academy.";
+>>>>>>> Stashed changes
 
             if (allow)
             {
@@ -115,7 +143,11 @@ public class Text : MonoBehaviour
         }
         else if (UI == 94)
         {
+<<<<<<< Updated upstream
             info.text = "Did you know 'interesting facts about certain field Three part two' how fasinating.";
+=======
+            info.text = "Did you know QUT offers 9 different arts degrees, involving acting, animation, creative writing, dance, drama, film screen and new media, music, technical production and visual arts.";
+>>>>>>> Stashed changes
             if (allow)
             {
                 t = 0;
@@ -124,6 +156,7 @@ public class Text : MonoBehaviour
         }
         else if (UI == 93)
         {
+<<<<<<< Updated upstream
             info.text = "Did you know 'interesting facts about certain field Four' how fasinating.";
 
             if (allow)
@@ -135,6 +168,9 @@ public class Text : MonoBehaviour
         else if (UI == 92)
         {
             info.text = "Mini Game three Coming soon!!!";
+=======
+            info.text = "Did you know QUT offers 9 different arts degrees, involving acting, animation, creative writing, dance, drama, film screen and new media, music, technical production and visual arts.";
+>>>>>>> Stashed changes
 
             if (allow)
             {
